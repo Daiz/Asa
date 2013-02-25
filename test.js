6c79a029ed8402916d1711bd1f3fa0a30adbea79
Daiz <taneli.vatanen@gmail.com>
2013-02-25 14:39:50 +0200

    Clean up test.js

--- test.js
+++ test.js
@@ -1,22 +1,10 @@
 /*jshint node:true */
 
 var express = require("express"),
-		//vidStreamer = require("vid-streamer"),
 		app = express();
 
 app.use(express.logger('dev'));
-//app.use(app.router);
 app.use(express.static(__dirname+"/test"));
-/*app.use(express.static("/subs",__dirname+"/subs"));
-app.get("/video", vidStreamer.settings({
-	"mode": "development",
-	"forceDownload": false,
-	"random": false,
-	"rootFolder": "/video/",
-	"rootPath": "video/",
-	"server": "VidStreamer"
-}));
-//app.use(app.router);*/
 
 app.listen(3000);
 console.log("Listening on port 3000");
